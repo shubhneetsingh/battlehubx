8e7433d28aeb4b312e18d43ba932b52006e2eda4
shubhneetsingh <141572676+shubhneetsingh@users.noreply.github.com>
2025-08-06 20:53:28 +0530

    Update next.config.js

--- battlehubx/next.config.js
+++ battlehubx/next.config.js
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
-
-// next.config.js
-module.exports = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: false,
-  },
-};
-
-// next.config.js
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: false,
-  },
-};
-
-module.exports = nextConfig;
-
